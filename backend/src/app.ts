27cbf3fdbedd360a65892740ea819decd89a26d6
Giovanni <giovannicruzvinhedo@gmail.com>
2025-05-06 20:14:48 -0300

    feat: add frontend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,11 @@
 import express from "express"
+import cors from "cors"  // Importando o pacote CORS
 import pokemonRoutes from "./routes/pokemonRoutes"
 
 const app = express()
+
+// Configurando o CORS
+app.use(cors())  // Permite todas as origens. Pode ser personalizado conforme necessário
 
 app.use(express.json())
 app.use("/api", pokemonRoutes)
---
+++ frontend/pokedex-plus/app/page.tsx
@@ -0,0 +1,59 @@
+"use client"
+
+import { useEffect, useState } from "react"
+import axios from "axios"
+import Link from "next/link"
+
+interface Pokemon {
+  name: string
+  id: number
+  type: string
+  image: string
+}
+
+const Home = () => {
+  const [pokemons, setPokemons] = useState<Pokemon[]>([])
+
+  useEffect(() => {
+    const fetchPokemons = async () => {
+      try {
+        const response = await axios.get<{ data: Pokemon[] }>(
+          "http://localhost:5000/api/pokemon"
+        )
+        // Acesse o array dentro de response.data
+        setPokemons(response.data.data)
+      } catch (error) {
+        console.error("Error fetching pokemons", error)
+      }
+    }
+
+    fetchPokemons()
+  }, [])
+
+  return (
+    <div className="container mx-auto p-6">
+      <h1 className="text-3xl font-bold text-center mb-6">PokeDex+</h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+        {pokemons.map((pokemon) => (
+          <div
+            key={pokemon.id}
+            className="bg-white shadow-md rounded-lg p-4 text-center"
+          >
+            <img
+              src={pokemon.image}
+              alt={pokemon.name}
+              className="w-32 h-32 mx-auto mb-4"
+            />
+            <h2 className="text-xl font-semibold">{pokemon.name}</h2>
+            <p className="text-sm text-gray-600">{pokemon.type}</p>
+            <Link href={`/pokemon/${pokemon.name}`} className="text-blue-500">
+              View Details
+            </Link>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default Home
